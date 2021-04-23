e06027b8ecd740408c5fc9ecda55bf7862b7afb5
nickschmitt <nickschmitt@gmail.com>
2021-04-23 12:39:45 -0700

    Sum of Multiples | Rust

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,10 +1,4 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-
-    factors.into_iter().collect()
-
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    (1..limit)
+    .filter(|num| factors.iter().any(|factor| *factor > 0 && num % factor == 0)).sum()
 }

10fb6c5d68118259132f7862476193fc1e805e61
nickschmitt <nickschmitt@gmail.com>
2021-05-15 12:30:42 -0700

    Complete space age exercise in Javascript
    The ORBITALS constant is assigned to an associative array of planets
    and orbital periods relative to Earth years. The
    SECONDS_IN_YEARS constant represents the number of seconds in years.
    The age function takes a planet and age provided by the user. The
    planet is sanitized to lowercase, then handled for errors. If the
    user input is "pluto" an error is thrown reminding the user that
    Pluto is not a planet. If the user input is anything else not in
    the ORBITALS associative array, then a generic error is thrown.
    
    Otherwise, the age is divided by the relative orbital period, which
    represents the age of someone on a particular planet in seconds.
    This value is then divided by the number of seconds in a year to
    attain the age in years rather than seconds. This is then cast to
    a two-decimal string with toFixed, and cast back to a floating
    point value with Number(). This number is then returned from the
    function.

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,25 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const ORBITALS = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+const SECONDS_IN_YEARS = 31_557_600
+
+export const age = (planet, age) => {
+    let input = planet.toLowerCase()
+    if (!(input in ORBITALS)) {
+        if (input === 'pluto') {
+            throw new Error('Pluto is not a planet :(')
+        } else {
+            throw new Error('That is not a planet')
+        }
+    } else {
+        return Number((age / ORBITALS[input] / SECONDS_IN_YEARS).toFixed(2))
+    }
+}
